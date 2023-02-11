89fb6f94bbae4528be9a43b47d7fea607a606e95
LarsonRagnar <larionov1992@gmail.com>
2023-02-11 17:09:31 +0300

    GPTтреугольник

---
+++ 3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+ 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j == 1 || j == i || i == n)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
